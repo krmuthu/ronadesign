9980d134b36ea0f66b14f9b1848c5bce25ebede8
krmuthu <mk.kraam@gmail.com>
2020-08-24 09:53:18 +0000

    image added

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../layout'
+
+import '../css/about-us.css'
+
 export default function Index() {
   return (
     <Layout>
-      <div></div>
-      coming soon
+        <div class="about">
+      <h1>We</h1>
+      <p>Are a team of web designers, web developers, and writers who have worked with some of the world’s leading brands, building a portfolio of great web pages and products. Ronadesign is a creation of our part-time efforts to bring our expertise to the rest of the world.</p>
+      <h1>Our Commitment</h1>
+      <p>Is to help your organization through the visual transformation of your online presence to match what your brand deserves - a world-class design. We will also ensure that this transformation isn’t so that you look pretty since our design philosophy entails that you look great AND convert.</p>
+      </div>
     </Layout>
   )
 }
