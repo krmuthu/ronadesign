db4bfbcfb447a6452134173cc636f8cfa68d3ccf
Muthukumar Kr <mk.kraam@gmail.com>
2020-09-12 15:17:08 +0800

    Update works

--- src/layout.js
+++ src/layout.js
@@ -1,23 +1,15 @@
 import React, { useEffect } from 'react'
 import { Link, navigate } from 'gatsby'
 import { Helmet } from 'react-helmet'
-import { globalHistory as history } from '@reach/router'
 
 import './css/typography.css'
 import './css/styles.css'
-import logo from './images/logo.png'
+import logo from './images/rlogo.png'
 
 
 export default function Template({path, children, currentPage }) {  
   const [menustate, setMenuState] = React.useState(false);
-  const { location } = history;
-  let next = 0;
-  let curPage = 0;
-  const pages = ['/', '/about-us/', '/works/', '/contact-us/'];
-  let curPageNum = pages.findIndex((e)=>{return e.includes(location.pathname)})
-  let isScrolling = true;
-  let timer;
-  let clientY;
+  
 
 /*
   useEffect(() => {
@@ -91,85 +83,6 @@ export default function Template({path, children, currentPage }) {
     
     
   })*/ 
-
-
-  const goPage = () => {
-    console.log(curPageNum)
-    isScrolling = true;
-    setTimeout(() => { isScrolling = false; }, 1000);
-    navigate(pages[curPageNum]);
-    return;
-    //document.querySelectorAll('.active').forEach((e) => {console.log(e)})
-    const aciveScreen = document.querySelector('.active');
-    if(aciveScreen) {
-      aciveScreen.classList.remove('active');
-    }
-    const newScreen = document.querySelector('.' + pages[curPage]);
-    if(aciveScreen) {
-      newScreen.classList.add('active');
-    }
-    
-    
-
-  }
-
-  const onScroll = (e) => {
-    return;
-    console.log(e)
-    if (isScrolling) return;
-    next += e.deltaY;
-    if (next > 2) {
-      next = 0;
-      if (curPageNum < 3){
-        curPageNum++;
-        goPage();
-      }
-      
-    } if (next < -2) {
-      next = 0;
-      if (curPageNum > 0){
-        curPageNum--;
-        goPage();
-      }
-    }
-
-  }
-
-  const touchEndFn = (e) => {
-    let deltaY;
-    // Compute the change in X and Y coordinates. 
-    // The first touch point in the changedTouches
-    // list is the touch point that was just removed from the surface.
-    
-    deltaY = e.changedTouches[0].clientY - clientY;
-    if(deltaY < 0) {
-      if(window.innerHeight + window.scrollY >= document.body.scrollHeight){
-        curPageNum++;
-        goPage();
-      }
-    }
-    // Process the data ... 
-  }
-
-  const touchStartFn = (e) => {
-    // Cache the client X/Y coordinates
-    clientY = e.touches[0].clientY;
-
-  }
-
-  useEffect(() => {
-    //window.addEventListener('wheel', onScroll, false);
-    document.addEventListener('touchstart', touchStartFn, false);
-    
-    document.addEventListener('touchend', touchEndFn, false);
-    
-    timer = setTimeout(() => { isScrolling = false; }, 1000);
-    return () => {
-        clearTimeout(timer);
-        document.removeEventListener("touchmove", onScroll, {passive: false});
-        //window.removeEventListener('wheel', onScroll, false);
-    }
-  })
   
   const onChangeMenu = (e) => {
     setMenuState(!menustate);
@@ -185,7 +98,7 @@ export default function Template({path, children, currentPage }) {
         <div className="grid">
           <div className="sidebar">
             <div className="logo">
-            <Link to="/"><img src={logo} alt="Rona Design" height="50px" width="50px" /><span>Rona design</span></Link>
+            <Link to="/"><div className="imgHolder"><img src={logo} alt="Rona Design"  /></div><span>Rona design</span></Link>
             </div>
             <div className="nav">
               <input type="checkbox"  checked={menustate} onChange={onChangeMenu}  id="navbar-checkbox" className="navbar-checkbox" name="navbar-checkbox" />
